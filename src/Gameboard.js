9f016f3538996b77348f987360a5cd241f3f8964
chrisdadev13 <chrisdadev13@gmail.com>
2022-09-20 18:45:53 -0400

    Receive attack function and test (building...)

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,7 +1,8 @@
 const Gameboard = () => {
-  const COLS = 10;
-  const ROWS = 10;
-  let board = [];
+  const COLS = 2;
+  const ROWS = 2;
+
+  board = COLS * ROWS;
 
   const createBoard = () => {
     for(let i = 0; i < COLS * ROWS; i++){
@@ -15,7 +16,7 @@ const Gameboard = () => {
     if(ship.direction == 'row'){
       for(let i = coordinate; i < ship.long + coordinate; i++){
         board[i] = ship.name;
-        ship.position.push(board[i]);
+        ship.position.push(i);
       }
     }
     return board;
@@ -26,13 +27,32 @@ const Gameboard = () => {
     if(ship.direction == 'col'){
       for(let i = coordinate; i < ship.long + COLS; i = i + ROWS){
         board[i] = ship.name;
-        ship.position.push(board[i]);
+        ship.position.push(i);
       }
     }
     return board;
   }
 
-  return { rowShip, colShip };
+  const placeShip = (ship, coordinate, direction) => {
+    if(direction == 'row'){
+      return rowShip(ship, coordinate);
+    }else if(direction == 'col'){
+      return colShip(ship, coordinate);
+    }
+  }
+
+  const receiveAttack = (target, ship) => {
+    for(let i = 0; i < ship.position.length; i++){
+      if(i == target && typeof board[i] != 'string'){
+        ship.hit(target);
+      }else{
+        board[i] = 'missed';
+      }
+    }
+    return board;
+  }
+
+  return { rowShip, colShip, placeShip, receiveAttack };
 }
 
 module.exports = Gameboard;
