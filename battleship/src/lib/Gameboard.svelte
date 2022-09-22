6a1638cb4b79d2ef3a6917fdd778d1cbbf69c420
chrisdadev13 <chrisdadev13@gmail.com>
2022-09-21 21:44:13 -0400

    UI building

---
+++ battleship/src/lib/Gameboard.svelte
@@ -0,0 +1,62 @@
+<script>
+  const WIDTH = 10;
+  const HEIGHT = 10;
+  let board = [];
+  const createBoard = () => {
+    for(let i = 0; i < WIDTH * HEIGHT; i++){
+      board.push(i);
+
+    }
+  }
+  createBoard();
+
+  $:board[0] = 'Missed'
+  $:board[1] = 'Missed'
+
+</script>
+
+<main class="board-container">
+  {#each board as tile}
+    {#if typeof tile == 'number'}
+      <div class="sea-container board-tile" />
+    {:else if typeof tile == 'string' && tile == 'Missed'}
+      <div class="missed-container">
+        X
+      </div>
+    {:else if typeof tile == 'string' && tile != 'Missed'}
+      <div class="ship-container" />
+    {/if}
+  {/each}
+</main>
+
+<style>
+  .board-container{
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+  }
+
+  .sea-container{
+    padding: 20px;
+    background-container: #ffffff;
+  }
+
+  .missed-container{
+    padding: 20px;
+    border: 1px solid red;
+    background-color: #f2f4f8;
+    font-size: 48px;
+    color: red;
+  }
+
+  .ship-container{
+    padding: 20px;
+    background-color: #f2f2ff; 
+    border: 1px solid #0000ff;
+  }
+
+  .board-tile{
+    border: 1px solid #b4b4ff;
+    padding: 20px;
+  }
+</style>
