4fc29e312842414b17356023d82cdf17ab914fc4
chrisdadev13 <chrisdadev13@gmail.com>
2022-09-24 19:45:15 -0400

    User interface (building...)

---
+++ src/lib/PlayerBoard.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Board from '../scripts/Gameboard';
+  import Ship from '../scripts/Ship';
+  import Player from '../scripts/Player';
+
+  let gameBoard = new Board; 
+  let submarine = new Ship(3, [], true, false);
+  let carrier = new Ship(5, [], false, false);
+
+  const placeShipOnBoard = (event) => {
+    let target = parseInt(event.target.id);
+    gameBoard.placeShip(submarine, target);
+    console.log(submarine);
+    gameBoard = gameBoard;
+  }
+</script>
+
+<main class="board-container">
+  {#each gameBoard.board as tile}
+    {#if tile.status == 0}
+      <div class="sea-tile" id={tile.index} on:click={event => placeShipOnBoard(event)}>
+      </div> 
+    {:else if tile.status != 0}
+      <div class="ship-tile">
+      </div> 
+    {/if}
+ {/each}
+</main>
+
+<style>
+  .board-container{
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+  }
+  .sea-tile{
+    border: 1px solid white;
+    width: 40px;
+    height: 40px;
+  }
+  .ship-tile{
+    border: 1px solid white;
+    width: 40px;
+    height: 40px;
+    background-color: teal;
+  }
+</style>
