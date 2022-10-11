56c9a8947af87806da835d709b0d17761e81cf0f
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-11 14:00:29 -0400

    Computer board component

---
+++ src/components/EnemyBoardUI.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import Gameboard from "../scripts/gameboard";
+  import Ship from "../scripts/ship";
+
+  export let board;
+
+</script>
+
+
+<main class="container">
+  <div class="board-container">
+    {#each board.board as row, rowIndex}
+      {#each row as col, colIndex}
+        {#if board.board[rowIndex][colIndex] == 0}
+          <div 
+            class={"empty-tile"}
+            data-row={rowIndex} 
+            data-col={colIndex}
+          >
+          </div> 
+        {:else if board.board[rowIndex][colIndex] == 1 || typeof board.board[rowIndex][colIndex] == "object"}
+          <div 
+            class={"empty-tile"}
+            data-row={rowIndex} 
+            data-col={colIndex}
+          >
+          </div> 
+        {:else if board.board[rowIndex][colIndex] == "X"} 
+          <div 
+            class={"missed-tile"} 
+            data-row={rowIndex} 
+            data-col={colIndex}
+          >
+            *
+          </div> 
+        {:else if board.board[rowIndex][colIndex] == "Hitted"}
+          <div 
+            class={"hitted-tile"} 
+            data-row={rowIndex} 
+            data-col={colIndex}
+          >
+           X 
+          </div>
+        {/if}
+      {/each}
+    {/each}
+  </div>  
+</main>
+
+<style>
+  .board-container{
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+    width: 350px;
+    height: 350px;
+    border: 2px solid #b4b4ff;
+    margin-right: 15px;
+  }
+  .empty-tile{
+    width: full;
+    height: full;
+    border: 1px solid #b4b4ff;
+    cursor:crosshair;
+  }
+  .missed-tile{
+    width: full;
+    height: full;
+    border: 1px solid #b4b4ff;
+    background-color: #fafad2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 12px;
+  }
+  .hitted-tile{
+    width: full;
+    height: full;
+    border: 1px solid #b4b4ff;
+    background-color: #fff2f2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 12px;
+  }
+  .ship-tile{
+    width: full;
+    height: full;
+    border: 1px solid #0000ff;
+    background-color: #f2f2ff;
+  }
+</style>
