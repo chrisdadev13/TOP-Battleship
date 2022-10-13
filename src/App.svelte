52fcecb8a30c91f9f98d5438fc54beaec09dce04
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-13 16:26:43 -0400

    Game finished (beta)

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,57 @@
 <script lang="ts">
-  import BoardUi from "./components/BoardUI.svelte";
+  import BoardUI from "./components/BoardUI.svelte";
   import EnemyBoardUi from "./components/EnemyBoardUI.svelte";
   import Gameboard from "./scripts/gameboard";
   import Ship from './scripts/ship';
 
-  let carrier = new Ship(4, [], false);
-
   let userBoard = new Gameboard;
   let computerBoard = new Gameboard;
-  computerBoard.receiveAttack(5, 0);
-  computerBoard.placeShip(carrier, 3, 0, false)
-  console.log(computerBoard);
-  computerBoard.receiveAttack(3, 0);
+  let userTurn = false;
+  let started = false;
+
+  const hideBtn = () => {
+    document.querySelector("#init-btn-container").remove();
+    userTurn = true;
+    started = true;
+    computerBoard.placeRandomly();
+    computerBoard = computerBoard;
+  }
+
+  const userAttack = (event) => {
+    if(userTurn == true){
+      let row = parseInt(event.target.getAttribute("data-row"));  
+      let col = parseInt(event.target.getAttribute("data-col"));
+      if(computerBoard.board[row][col] == 0 || computerBoard.board[row][col] == 1 || typeof computerBoard.board[row][col] == "object"){
+        computerBoard.receiveAttack(row, col);
+        computerBoard = computerBoard;
+        userTurn = false;
+      }else{
+        userTurn = true;
+      }
+    }
+  }
+
+  const computerAttack = () => {
+    let row = Math.floor(Math.random() * 10);
+    let col = Math.floor(Math.random() * 10);
+
+    if(started == true){
+      if(userTurn == false){
+        userBoard.receiveAttack(row, col)
+        userBoard = userBoard;
+        userTurn = true;
+      }
+    }
+  }
+
+  $: if(userTurn == false){
+    computerAttack();
+  }
 </script>
 
 <main>
-  <BoardUi board={userBoard}/>
-  <EnemyBoardUi board={computerBoard}/>
+  <BoardUI board={userBoard} hideBtn={hideBtn}/>
+  <EnemyBoardUi board={computerBoard} attackBoard={(event) => userAttack(event)} />
 </main>
 
 <style>
